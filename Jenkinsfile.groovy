04585a9a17d96ac00ff29e82113580baa16bc398
sstanytska <stanytskasofia@gmail.com>
2019-03-16 11:04:30 -0500

    more instrackted added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,23 @@
+/*Works for Jenkins */
 node {
-    stage("Pull git")
+    stage("Pull git"){
       git "git@github.com:sstanytska/pipeline.git"
+    }
+
+    stage("Install apache"){
+      sh "ssh ec2-user@${DEVIP}  sudo yum install httpd -y"
+    }
+
+    stage("Start Appache"){
+      sh "ssh ec2-user@${DEVIP}  sudo systemctl start httpd"
+    }
+
+    stage("Copy files"){
+      sh "rsync -aP --delete index.html  ec2-user@${DEVIP}:/tmp"
+    }
+
+    stage("Move files"){
+      sh "ssh ec2-user@${DEVIP} sudo cp -f /tmp/index.html /var/www/html/index.html"
+    }
+
 }
